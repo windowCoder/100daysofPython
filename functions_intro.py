fedd7bf6cb8b0b5a8e353c9f4692209cff292b1a
Matthew <40544801+windowCoder@users.noreply.github.com>
2021-09-09 04:49:14 -0700

    Add files via upload

---
+++ functions_intro.py
@@ -0,0 +1,13 @@
+your_name = input("What is your name? ")
+your_loc = input("What is your location ")
+def greet_with(name, loc):
+    print(f"Hello, {name}! What is it like in {your_loc}?\n")
+greet_with(your_name, your_loc)
+
+def simple_math(a,b,c):
+    print("\nSimple Math Time!")
+    print(a+2)
+    print(b*2)
+    print(c/2)
+simple_math(a=2, b=4, c=6)
+
